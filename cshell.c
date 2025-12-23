16d9cb78ad169bd6ad37a6c4c1be1a971f777627
Ziad Ijja <ijjaziad@gmail.com>
2025-12-23 13:52:22 +0100

    feat: handle semicolon (;) so now you can do multiple commands

--- cshell.c
+++ cshell.c
@@ -8,10 +8,22 @@
 
 #define BUFFER_SIZE 1024 
 #define TOKEN_DELIMITER " \t" // set of delimiters
-
+#define MAX_CMDS 10
 
 #define DELETE_CHAR 127
 #define ESC_CHAR 27
+
+
+/* command representation (multiple arguments) */
+typedef struct  {
+    char ** argv;
+} Command ;
+
+/* line of multiple commands, needed for ; usage & maybe more after */
+typedef struct {
+    Command* cmds;
+    int totalcmds;
+} Line ;
 
 struct termios orig_termios;
 
@@ -32,9 +44,9 @@ void enable_raw_mode() {
 // methods enumeration
 void loop();
 char * read_line();
-char ** parse_line(char * line);
-void print_words(char ** words);
-int exec_word(char ** words);
+Line parse_line(char * line);
+void print_words(Line words);
+int exec_word(Command command);
 
 // built-in methods
 int exec_exit(char ** args);
@@ -44,12 +56,17 @@ int exec_cd(char ** args);
 
 
 /* Print all the words in a list */
-void print_words(char **words) {
-    int i = 0;
-    while (words[i] != NULL) {
-        printf("%s\n", words[i]);
-        i++;
+void print_words(Line words) {
+    int i ;
+
+    for (int tcmds = 0 ; tcmds <= words.totalcmds ; ++ tcmds) {
+        i = 0;
+        while (words.cmds[tcmds].argv[i] != NULL) {
+            printf("%s\n", words.cmds[tcmds].argv[i]);
+            i++;
+        }
     }
+    
 }
 
 /* Read a line from the standard input */
@@ -121,27 +138,56 @@ char * read_line() {
 }
 
 /* Parse a line into tokens */
-char ** parse_line( char * line) {
+Line parse_line( char * line) {
     int buffer_size = BUFFER_SIZE ;
     int position = 0 ;
 
     char * t ;
-    char ** tokens = malloc(sizeof(char*) * buffer_size);
+    //char ** tokens = malloc(sizeof(char*) * buffer_size);
+    Line pline;
+    pline.cmds = malloc(sizeof(Command) * MAX_CMDS);
+    pline.totalcmds = 0 ;
+    pline.cmds[pline.totalcmds].argv = malloc(sizeof(char*) * buffer_size);
 
-    if (!tokens) {
+
+    if (!pline.cmds[pline.totalcmds].argv) {
         fprintf(stderr, "error: Allocation error\n");
         exit(EXIT_FAILURE);
     }
 
     t = strtok(line, TOKEN_DELIMITER) ;
     while (t != NULL) {
-        tokens[position++] = t ;
+
+        if (strcmp(t, ";") == 0) {
+            
+            pline.cmds[pline.totalcmds].argv[position] = NULL ;
+            
+            // reset values for new cmd
+            position = 0 ;
+            buffer_size = BUFFER_SIZE ;
+            pline.totalcmds ++ ;
+
+            if (pline.totalcmds >= MAX_CMDS) {
+                fprintf(stderr, "error: too many commands\n");
+                exit(EXIT_FAILURE);
+            }
+
+            pline.cmds[pline.totalcmds].argv = malloc(sizeof(char*) * buffer_size);
+
+            if (!pline.cmds[pline.totalcmds].argv) {
+                fprintf(stderr, "error: Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+
+        } else {
+            pline.cmds[pline.totalcmds].argv[position++] = t ;
+        }
         
         if (position >= buffer_size) {
             buffer_size += BUFFER_SIZE ;
-            tokens = realloc(tokens,  sizeof(char * )* buffer_size);
+            pline.cmds[pline.totalcmds].argv = realloc(pline.cmds[pline.totalcmds].argv,sizeof(char * )* buffer_size);
 
-            if (!tokens) {
+            if (!pline.cmds[pline.totalcmds].argv) {
                 fprintf(stderr, "error: Allocation error\n");
                 exit(EXIT_FAILURE);
             }
@@ -152,36 +198,40 @@ char ** parse_line( char * line) {
     }
 
     // to ensure the end of the word
-    tokens[position] = NULL ;
-    return tokens ;
+    pline.cmds[pline.totalcmds].argv[position] = NULL ;
+    pline.totalcmds++;
+    return pline ;
 
 }
 
 /* Execute a command */
-int exec_words(char ** words) {
+int exec_words(Command command) {
     pid_t child_pid ;
     int exec_status ;
 
-    if (strcmp(words[0],"exit") == 0) {
-        return exec_exit(words);
-    }
-    if (strcmp(words[0],"cd") == 0) {
-        return exec_cd(words);
-    }
-
-    child_pid = fork() ;
-
-    if (child_pid == 0) {
-        if (execvp(words[0], words) < 0) {
-            printf("error: command doesn't exist : %s\n", words[0]);
-            exit(EXIT_FAILURE);
+   
+        if (strcmp(command.argv[0],"exit") == 0) {
+            return exec_exit(command.argv);
         }
-    } else if (child_pid == -1) {
-        fprintf(stderr, "error: process creation error\n");
-        exit(EXIT_FAILURE);
-    } else {
-        waitpid(child_pid, &exec_status, 0) ; // 0 since we don't need any option for now
-    }
+        if (strcmp(command.argv[0],"cd") == 0) {
+            return exec_cd(command.argv);
+        }
+
+        child_pid = fork() ;
+
+        if (child_pid == 0) {
+            if (execvp(command.argv[0], command.argv) < 0) {
+                printf("error: command doesn't exist : %s\n", command.argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        } else if (child_pid == -1) {
+            fprintf(stderr, "error: process creation error\n");
+            exit(EXIT_FAILURE);
+        } else {
+            waitpid(child_pid, &exec_status, 0) ; // 0 since we don't need any option for now
+        }
+    
+    
 
     return 1 ;
 
@@ -212,7 +262,7 @@ int exec_cd(char ** words) {
 /* Loop to read and execute commands */
 void loop() {
     char * line ;
-    char ** words ; // parsed line
+    Line words ; // parsed line
     int status = 1 ;
     
 
@@ -227,13 +277,22 @@ void loop() {
         enable_raw_mode() ;
         line = read_line() ;
         disable_raw_mode() ;
+        //printf("line is : %s",line);
         words = parse_line(line) ;
         //print_words(words) ;
-        if (words[0] != NULL) {
-            status = exec_words(words);
+        
+        for (int tcmds = 0 ; tcmds <= words.totalcmds ; ++tcmds) {
+            if (words.cmds[tcmds].argv[0] != NULL) {
+                status = exec_words(words.cmds[tcmds]);
+            }
         }
+        
+        for (int tcmds = 0 ; tcmds < words.totalcmds ; ++tcmds) {
+            free(words.cmds[tcmds].argv);
+        }
+        free(words.cmds);
         free(line);
-        free(words);
+        
 
     } while (status != 0) ;
 
